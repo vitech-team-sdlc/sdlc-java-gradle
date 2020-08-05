3f050a73e5063b9cb9d731f8ec07b7e2bac2ac7e
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-05 14:54:32 +0300

    Added SpringBoot dependency and main class to the project.

---
+++ src/main/java/com/vitech/moodfeed/MoodFeedApplication.java
@@ -0,0 +1,13 @@
+package com.vitech.moodfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodFeedApplication.class, args);
+    }
+
+}
