07d52344954457a84c557ed6482345c982ee3f00
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-06 22:47:50 +0300

    Updated user service to use users stored in DB.

---
+++ src/main/java/com/vitech/moodfeed/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.vitech.moodfeed.service;
+
+import com.google.common.collect.Lists;
+import com.vitech.moodfeed.domain.User;
+import com.vitech.moodfeed.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Implementation of {@link UserService} interface
+ */
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private static final Random RANDOM = new Random();
+    private final UserRepository userRepository;
+
+    @Override
+    public User getLoggedUser() {
+        // get all users
+        List<User> allUsers = Lists.newArrayList(userRepository.findAll());
+        // return random one
+        return allUsers.get(RANDOM.nextInt(allUsers.size()));
+    }
+
+}
