797b0199cfceb3d80926ab654b32ca2eca423d3c
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-07 14:39:24 +0300

    Enhanced messages response with user info and added limit on number of messages.

--- src/main/java/com/vitech/moodfeed/service/impl/MessageServiceImpl.java
+++ src/main/java/com/vitech/moodfeed/service/impl/MessageServiceImpl.java
@@ -1,25 +1,41 @@
 package com.vitech.moodfeed.service.impl;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.vitech.moodfeed.domain.Message;
+import com.vitech.moodfeed.domain.User;
 import com.vitech.moodfeed.dto.MessageResponse;
 import com.vitech.moodfeed.repo.MessageRepository;
+import com.vitech.moodfeed.repo.UserRepository;
 import com.vitech.moodfeed.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
 
+    private final UserRepository userRepository;
     private final MessageRepository messageRepository;
 
     @Override
     public List<MessageResponse> getMessages(int limit) {
-        // TODO: join user information
-        // TODO: add limit to query
-        return null;
+        // query messages
+        PageRequest pageRequest = PageRequest.of(0, limit, Sort.by("createdAt"));
+        List<Message> messages = Lists.newArrayList(messageRepository.findAll(pageRequest));
+        // query messages creators
+        List<Long> creatorIds = messages.stream().map(Message::getCreatorId).collect(Collectors.toList());
+        Map<Long, User> creators = Maps.uniqueIndex(userRepository.findAllById(creatorIds), User::getId);
+        // return messages enhanced with information about it's creators
+        return messages.stream()
+                .map(msg -> MessageResponse.from(creators.get(msg.getCreatorId()), msg))
+                .collect(Collectors.toList());
     }
 
     @Override
