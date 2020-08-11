02018227bcd68686090f93b56f5cb90c4ecdf820
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-11 20:46:26 +0300

    Created base class for medium tests.

---
+++ src/integrationTest/java/com/vitech/moodfeed/MediumTest.java
@@ -0,0 +1,52 @@
+package com.vitech.moodfeed;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.concurrent.TimeUnit;
+
+@Tag("mediumTest")
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
+public class MediumTest {
+
+    @LocalServerPort
+    private int port;
+
+    private static final TestRestTemplate REST_TEMPLATE = new TestRestTemplate();
+
+    @Container
+    private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:8");
+
+    @DynamicPropertySource
+    static void mySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+    }
+
+    protected String baseUrl() {
+        return "http://localhost:" + port;
+    }
+
+    protected TestRestTemplate restTemplate() {
+        return REST_TEMPLATE;
+    }
+
+    protected MySQLContainer mySQLContainer() {
+        return MY_SQL_CONTAINER;
+    }
+
+}
