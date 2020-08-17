5c0341b7e2dccf00f1b5daaeb154f9c4e553ba0a
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-17 15:15:28 +0300

    Removed useless cases on different limit from messages service test.

--- src/test/java/com/vitech/moodfeed/message/MessageServiceTest.java
+++ src/test/java/com/vitech/moodfeed/message/MessageServiceTest.java
@@ -7,8 +7,6 @@ import com.vitech.moodfeed.message.dto.MessageResponse;
 import com.vitech.moodfeed.user.User;
 import com.vitech.moodfeed.user.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.data.domain.PageImpl;
@@ -27,10 +25,6 @@ import static org.mockito.Mockito.when;
 
 public class MessageServiceTest extends SmallTest {
 
-    private static final int NUMBER_OF_MESSAGES = 5;
-    private static final int MESSAGES_LIMIT_LOWER = 4;
-    private static final int MESSAGES_LIMIT_HIGHER = 10;
-
     @Mock
     private UserRepository userRepositoryMock;
 
@@ -40,12 +34,10 @@ public class MessageServiceTest extends SmallTest {
     @InjectMocks
     private MessageServiceImpl messageService;
 
-    @ParameterizedTest
-    @ValueSource(ints = { MESSAGES_LIMIT_LOWER, MESSAGES_LIMIT_HIGHER })
+    @Test
     void testGetMessages(int messagesLimit) {
         // mock
-        int expectedNumberOfMessages = Math.min(messagesLimit, NUMBER_OF_MESSAGES);
-        List<Message> expectedMessages = TestData.messages().subList(0, expectedNumberOfMessages);
+        List<Message> expectedMessages = TestData.messages();
         Map<Long, User> users = Maps.uniqueIndex(TestData.users(), User::getId);
         expectedMessages.forEach(msg -> when(userRepositoryMock.findById(msg.getCreatorId()))
                 .thenReturn(Optional.of(users.get(msg.getCreatorId()))));
@@ -53,7 +45,7 @@ public class MessageServiceTest extends SmallTest {
         // test
         List<MessageResponse> actualMessages = messageService.getMessages(messagesLimit);
         // verify
-        assertEquals(expectedNumberOfMessages, actualMessages.size());
+        assertEquals(expectedMessages.size(), actualMessages.size());
         actualMessages.forEach(msg -> assertNotNull(msg.getCreator()));
     }
 
