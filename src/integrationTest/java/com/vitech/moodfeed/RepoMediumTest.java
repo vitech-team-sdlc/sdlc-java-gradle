1eb93b1945d6d151d8dcaf94a632d57c31b62d3c
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-17 16:05:29 +0300

    Added pagination test to repo medium base test.

--- src/integrationTest/java/com/vitech/moodfeed/RepoMediumTest.java
+++ src/integrationTest/java/com/vitech/moodfeed/RepoMediumTest.java
@@ -1,40 +1,60 @@
 package com.vitech.moodfeed;
 
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @DataJdbcTest
 @RequiredArgsConstructor
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class RepoMediumTest<T> extends MediumTest {
 
-    private final CrudRepository<T, Long> crudRepository;
+    private final CrudRepository<T, Long> crudRepo;
     private final CrudTestDataProvider<T> crudTest;
 
     @Test
     void testCRUD() {
         // store new entity
-        T storedEntity = crudRepository.save(crudTest.entityProvider.get());
+        T storedEntity = crudRepo.save(crudTest.entityProvider.get());
         Long id = crudTest.idProvider.apply(storedEntity);
-        assertEquals(Optional.of(storedEntity), crudRepository.findById(id));
+        assertEquals(Optional.of(storedEntity), crudRepo.findById(id));
         // update stored entity
         T updatedEntity = crudTest.updatedEntityProvider.apply(id);
-        crudRepository.save(updatedEntity);
-        assertEquals(Optional.of(updatedEntity), crudRepository.findById(id));
+        crudRepo.save(updatedEntity);
+        assertEquals(Optional.of(updatedEntity), crudRepo.findById(id));
         // delete stored entity
-        crudRepository.deleteById(id);
-        assertFalse(crudRepository.findById(id).isPresent());
+        crudRepo.deleteById(id);
+        assertFalse(crudRepo.findById(id).isPresent());
+    }
+
+    @Test
+    void testPagination() {
+        // check if we need to test pagination logic
+        assumeTrue(crudRepo instanceof PagingAndSortingRepository);
+        PagingAndSortingRepository<T, Long> pagingSortingRepo = (PagingAndSortingRepository<T, Long>) crudRepo;
+        // store few entities
+        IntStream.range(0, 5).forEach((i) -> pagingSortingRepo.save(crudTest.entityProvider.get()));
+        // test pagination
+        PageRequest pageRequest = PageRequest.of(0, 2);
+        Page<T> pageResponse = pagingSortingRepo.findAll(pageRequest);
+        assertEquals(5, pageResponse.getTotalElements());
+        assertEquals(2, Lists.newArrayList(pageResponse).size());
     }
 
     @Value
