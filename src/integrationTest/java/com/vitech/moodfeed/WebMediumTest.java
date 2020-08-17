8561c9a81a6283c7f7a493398343de0c5b88e1b6
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-17 13:16:29 +0300

    Added logic to keep same DB state on each IT test case.

--- src/integrationTest/java/com/vitech/moodfeed/WebMediumTest.java
+++ src/integrationTest/java/com/vitech/moodfeed/WebMediumTest.java
@@ -1,23 +1,44 @@
 package com.vitech.moodfeed;
 
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Objects;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebMediumTest extends MediumTest {
 
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-    private static final TestRestTemplate REST_TEMPLATE = new TestRestTemplate();
+    private Liquibase liquibase;
 
-    protected String baseUrl() {
-        return "http://localhost:" + port;
+    @BeforeEach
+    public void beforeEach(
+            @Autowired DataSource dataSource,
+            @Value("${spring.liquibase.change-log}") String sqlChangeLog) throws Exception {
+        initLiquibase(dataSource.getConnection(), sqlChangeLog);
+        liquibase.dropAll();
+        liquibase.update((String) null);
+    }
+
+    private void initLiquibase(Connection connection, String sqlChangeLog) throws LiquibaseException {
+        if (Objects.isNull(liquibase)) {
+            liquibase = new Liquibase(sqlChangeLog, new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
+        }
     }
 
     protected TestRestTemplate restTemplate() {
-        return REST_TEMPLATE;
+        return restTemplate;
     }
 
 }
