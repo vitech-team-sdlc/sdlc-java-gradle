3fddb8c062bdd817a5808ae5ecffd6f695cffa08
Oleh Kovaliuk <oleh.kovaliuk@vitech.com.ua>
2020-08-27 14:06:19 +0300

    Added private constructor to Utils class.

--- src/main/java/com/vitech/moodfeed/utils/Utils.java
+++ src/main/java/com/vitech/moodfeed/utils/Utils.java
@@ -9,6 +9,9 @@ public class Utils {
 
     private static final Random RANDOM = new Random();
 
+    private Utils() {
+    }
+
     public static <T> T getRandom(Iterable<T> source) {
         List<T> sourceList = Lists.newArrayList(source);
         return sourceList.get(RANDOM.nextInt(sourceList.size()));
