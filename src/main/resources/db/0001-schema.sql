23bf1cc58b05cbdc898733d555e03d564ea76fb9
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-09-02 11:08:07 +0300

    * add actuator * change server port to 8080 * create HTTP file * add mysql to preview chart

--- src/main/resources/db/0001-schema.sql
+++ src/main/resources/db/0001-schema.sql
@@ -3,26 +3,26 @@
 
 CREATE TABLE user
 (
-    id          BIGINT         PRIMARY KEY AUTO_INCREMENT,
-    first_name  VARCHAR(64)    NOT NULL,
-    last_name   VARCHAR(64)    NOT NULL,
-    logo_color  VARCHAR(7)     NOT NULL DEFAULT '#000000'
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(64) NOT NULL,
+    last_name  VARCHAR(64) NOT NULL,
+    logo_color VARCHAR(7)  NOT NULL DEFAULT '#000000'
 );
 
 CREATE TABLE message
 (
-    id          BIGINT      PRIMARY KEY AUTO_INCREMENT,
-    message     LONGTEXT    NOT NULL,
-    creator_id  BIGINT      NOT NULL,
-    created_at  TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (creator_id) REFERENCES user(id)
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    message    LONGTEXT NOT NULL,
+    creator_id BIGINT   NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES user (id)
 );
 
 CREATE TABLE hashtag
 (
-    id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
-    message_id  BIGINT          NOT NULL,
-    tag         varchar(1024)   NOT NULL,
-    FOREIGN KEY (message_id)    REFERENCES message (id) ON DELETE CASCADE,
-    INDEX       (tag(32))
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    message_id BIGINT        NOT NULL,
+    tag        varchar(1024) NOT NULL,
+    FOREIGN KEY (message_id) REFERENCES message (id) ON DELETE CASCADE,
+    INDEX (tag(32))
 );
--- src/main/resources/db/0002-users.sql
+++ src/main/resources/db/0002-users.sql
@@ -1,13 +1,13 @@
 -- liquibase formatted sql
 -- changeSet users failOnError:true
 
-INSERT INTO user (first_name, last_name, logo_color) VALUES
-    ('Homer', 'Simpson', '#faf7af'),
-    ('Marge', 'Simpson', '#b167e3'),
-    ('Bart', 'Simpson', '#fad8af'),
-    ('Lisa', 'Simpson', '#f0c5eb'),
-    ('Ned', 'Flanders', '#afe0fa'),
-    ('Barney', 'Gumble', '#4002d8'),
-    ('Moe', 'Szyslak', '#1aa264'),
-    ('Seymour', 'Skinner', '#669bd9'),
-    ('Kent', 'Brockman', '#a3b8aa');
+INSERT INTO user (first_name, last_name, logo_color)
+VALUES ('Homer', 'Simpson', '#faf7af'),
+       ('Marge', 'Simpson', '#b167e3'),
+       ('Bart', 'Simpson', '#fad8af'),
+       ('Lisa', 'Simpson', '#f0c5eb'),
+       ('Ned', 'Flanders', '#afe0fa'),
+       ('Barney', 'Gumble', '#4002d8'),
+       ('Moe', 'Szyslak', '#1aa264'),
+       ('Seymour', 'Skinner', '#669bd9'),
+       ('Kent', 'Brockman', '#a3b8aa');
