03484c8fbb7cce5a1af9316c4c3900a9b3bb6600
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-09-03 16:29:09 +0300

    preview fix

--- src/main/java/com/vitech/moodfeed/MoodFeedApplication.java
+++ src/main/java/com/vitech/moodfeed/MoodFeedApplication.java
@@ -2,9 +2,6 @@ package com.vitech.moodfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoodFeedApplication {
