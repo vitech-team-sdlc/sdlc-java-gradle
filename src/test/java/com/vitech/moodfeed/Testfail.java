37a73a75275e4f10c1ffb7965ce511f967fa8d5b
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-09-04 11:56:34 +0300

    test fail

---
+++ src/test/java/com/vitech/moodfeed/Testfail.java
@@ -0,0 +1,13 @@
+package com.vitech.moodfeed;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class Testfail {
+
+    @Test
+    void testName() {
+        Assert.fail();
+    }
+}
