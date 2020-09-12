6f9ee601afede2a3fcf75974a11fb5a95699d3c4
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-09-12 11:01:50 +0300

    build: add sonar

---
+++ src/test/java/com/vitech/moodfeed/SmTest.java
@@ -0,0 +1,13 @@
+package com.vitech.moodfeed;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Tag("smTest")
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.Random.class)
+public class SmTest {
+}
