0c792ad3db5919fce27f59744ac40e7a237a39d4
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-09-14 14:58:46 +0300

    fix preview

--- src/main/java/com/vitech/moodfeed/MoodFeedApplication.java
+++ src/main/java/com/vitech/moodfeed/MoodFeedApplication.java
@@ -3,11 +3,24 @@ package com.vitech.moodfeed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+
 @SpringBootApplication
 public class MoodFeedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoodFeedApplication.class, args);
+    }
+
+    void YYYY(){
+
+        AtomicInteger aInt1 = new AtomicInteger(0);
+        AtomicInteger aInt2 = new AtomicInteger(0);
+
+        if (aInt1.equals(aInt2)) {
+
+        }
     }
 
 }
